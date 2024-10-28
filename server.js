00421487863f0394789257fe1086e64a41288add
Deep2410 <deep24102410@gmail.com>
2024-10-28 19:10:14 -0400

    Project section -> Authentication -> signup completed

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config();
+const dbConnect =  require('./database/db');
+const jwt = require('jsonwebtoken');
+const userModel = require('./models/user');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -11,7 +15,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 // app.use(bodyParser.urlencoded({ extended: true }));
+
+// ---------- for the email sender in contact me section ----------------------------
 
 app.post('/send-email', (req,res) => {
     const {name, email, message} = req.body;
@@ -47,3 +54,70 @@ app.post('/send-email', (req,res) => {
 app.listen(port, () => {
     console.log('server is running on port '+ port);
 })
+
+
+// ------------------------------------- Projects section ---------------------------------
+
+// ------- Database COnnection ---------------------------------------------
+
+dbConnect();
+
+// --- Authentication Application ---------------------------------------
+
+// --- password encryption function 
+
+const encryptPassword = async (password) => {
+    let salt = 10;
+    try{
+        let encryptedPassword = await bcrypt.hash(password, salt);
+        return encryptedPassword;
+    }
+    catch(error){
+        console.error(`error in password encryption: ${error}`);
+
+    }
+}
+
+// --- jwt authentication middleware
+
+const authentiacation = async (req, res, next) => {
+    const token = req.header('Authorization');
+
+    if(!token){
+        return res.status(401).json({message: 'Authorization denied!, no token found.'})
+    }
+
+    try{
+        console.log(token);
+        const actualToken = token.split(' ')[1];
+        const decodedToken = jwt.verify(actualToken, process.env.JWT_SECRET);
+        req.user = decodedToken.id;
+        next();
+    }
+    catch(error){
+        res.status(401).json({message: 'Tocken is not valid, you have to sign in again.'});
+    }
+}
+
+// --- signup request
+
+app.post('/signup', async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        const userExist = await userModel.findOne({email});
+
+        if(userExist){
+            return res.status(400).json({   : 'Account already exist.'});
+        }
+
+        const encryptedPassword = await encryptPassword(password);
+        const newUser = new userModel({name,email,password: encryptedPassword});//I have to use same name as userModel
+        await newUser.save();
+        res.status(201).json({message: 'User Registered Successfully'});
+    }
+
+    catch(error){
+        res.status(500).json({message: 'Error while Signing up:' + error});
+        console.log('Error1:' + error);
+    }
+});
